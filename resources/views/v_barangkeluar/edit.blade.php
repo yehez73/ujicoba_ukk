fc73bbdc321fee4c9593dbedda91d8a5f3ed90d7
yehez73 <wearyspider159357@gmail.com>
2024-06-21 00:15:55 +0700

    yyj

---
+++ resources/views/v_barangkeluar/edit.blade.php
@@ -0,0 +1,69 @@
+@extends('layouts.adm-main')
+
+@section('content')
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-body">
+                        <form action="{{ route('barangkeluar.update',$rsetBarangKeluar->id) }}" method="POST" enctype="multipart/form-data">                    
+                            @csrf
+                            @method('PUT')
+
+                            <div class="form-group">
+                                <label class="font-weight-bold">Tanggal Keluar</label>
+                                <input type="date" class="form-control @error('tgl_keluar') is-invalid @enderror" name="tgl_keluar" value="{{ old('tgl_keluar',$rsetBarangKeluar->tgl_keluar) }}" placeholder="Masukkan Nama Barang">
+                            
+                                <!-- error message untuk nama -->
+                                @error('tgl_keluar')
+                                    <div class="alert alert-danger mt-2">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+
+                            <div class="form-group">
+                                <label class="font-weight-bold">Quantity</label>
+                                <input type="text" class="form-control @error('qty_keluar') is-invalid @enderror" name="qty_keluar" value="{{ old('qty_keluar',$rsetBarangKeluar->qty_keluar) }}" placeholder="Masukkan Quantity">
+                            
+                                <!-- error message untuk qty_keluar -->
+                                @error('qty_keluar')
+                                    <div class="alert alert-danger mt-2">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+
+                            <div class="form-group">
+                                <label class="font-weight-bold">Barang</label>
+                                <select class="form-control @error('barang_id') is-invalid @enderror" name="barang_id">
+                                    <option value="">Pilih Barang</option>
+                                    @foreach($barangID as $barang)
+                                        <option value="{{ $barang->id }}" {{ old('barang_id', $rsetBarangKeluar->barang_id) == $barang->id ? 'selected' : '' }}>{{ $barang->merk }} - Stok : {{ $barang->stok }}</option>
+                                    @endforeach
+                                </select>
+
+                                @error('barang_id')
+                                    <div class="alert alert-danger mt-2">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+
+
+                            
+
+                            <button type="submit" class="btn btn-md btn-primary">SIMPAN</button>
+                            <button type="reset" class="btn btn-md btn-warning">RESET</button>
+
+                        </form> 
+                    </div>
+                </div>
+
+ 
+
+            </div>
+        </div>
+    </div>
+@endsection
